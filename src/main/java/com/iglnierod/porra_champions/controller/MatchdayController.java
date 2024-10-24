12d2e262377cb6162bd795324041e8a31616088a
iglnierod <iglesias.nieto.rodrigo.ald@gmail.com>
2024-10-24 15:56:36 +0200

    0.2: Calcular tabla de clasificación

--- src/main/java/com/iglnierod/porra_champions/controller/MatchdayController.java
+++ src/main/java/com/iglnierod/porra_champions/controller/MatchdayController.java
@@ -1,9 +1,14 @@
 package com.iglnierod.porra_champions.controller;
 
 import com.iglnierod.porra_champions.dto.MatchdayDTO;
+import com.iglnierod.porra_champions.model.Match;
 import com.iglnierod.porra_champions.model.Matchday;
+import com.iglnierod.porra_champions.model.Prediction;
+import com.iglnierod.porra_champions.model.User;
 import com.iglnierod.porra_champions.repository.MatchRepository;
 import com.iglnierod.porra_champions.repository.MatchdayRepository;
+import com.iglnierod.porra_champions.repository.PredictionRepository;
+import com.iglnierod.porra_champions.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
@@ -20,9 +25,15 @@ public class MatchdayController {
     @Autowired
     private MatchRepository matchrepository;
 
+    @Autowired
+    private PredictionRepository predictionRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping
     public List<Matchday> getMatchdays() {
-        return matchdayRepository.findAll(Sort.by(Sort.Direction.ASC,"id"));
+        return matchdayRepository.findAll(Sort.by(Sort.Direction.ASC, "id"));
     }
 
     @PostMapping
@@ -33,5 +44,38 @@ public class MatchdayController {
 
         Matchday savedMatchday = matchdayRepository.save(matchday);
         return ResponseEntity.ok(savedMatchday);
+    }
+
+    public void updateUserPointsForMatchday(Matchday matchday) {
+        List<Match> matches = matchrepository.findByMatchday(matchday);
+
+        for (Match match : matches) {
+            List<Prediction> predictions = predictionRepository.findByMatch_Id(match.getId());
+            int matchResult = checkMatchWinner(match.getLocalGoals(), match.getAwayGoals());
+            for (Prediction prediction : predictions) {
+                User user = getUser(match, prediction, matchResult);
+                userRepository.save(user);
+            }
+        }
+    }
+
+    private User getUser(Match match, Prediction prediction, int matchResult) {
+        int predictionResult = checkMatchWinner(prediction.getGoalsLocalPrediction(), prediction.getGoalsAwayPrediction());
+        int points = 0;
+        if (match.getLocalGoals() == prediction.getGoalsLocalPrediction() && match.getAwayGoals() == prediction.getGoalsAwayPrediction()) {
+            points = 3;
+        } else if (matchResult == predictionResult) {
+            points = 1;
+        }
+
+        User user = prediction.getUser();
+        user.setPoints(user.getPoints() + points);
+        return user;
+    }
+
+    private int checkMatchWinner(int localGoals, int awayGoals) {
+        if (localGoals == awayGoals) return 0; // Empate
+        if (localGoals > awayGoals) return 1; // Gana equipo local
+        else return 2; // Gana equipo visitante
     }
 }
